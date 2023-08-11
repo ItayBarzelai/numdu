53ea520c643db6ecf5252105d3ecb8d09c509e47
Itay Barzelai <itay1ba2@gmail.com>
2023-08-11 22:42:48 +0300

    added icons ts file

---
+++ icons.ts
@@ -0,0 +1,21 @@
+const ICONS = {
+    backgroundImage: require('numdu/assets/images/background-image.png'),
+    airplane: require('numdu/assets/images/airplane.png'),
+    arrow: require('numdu/assets/images/arrow.png'),
+    backspace: require('numdu/assets/images/backspace.png'),
+    bowAndArrow: require('numdu/assets/images/bow-and-arrow.png'),
+    coin: require('numdu/assets/images/coin.png'),
+    friend: require('numdu/assets/images/friend.png'),
+    global: require('numdu/assets/images/global.png'),
+    group: require('numdu/assets/images/group.png'),
+    lightning: require('numdu/assets/images/lightning.png'),
+    medal: require('numdu/assets/images/medal.png'),
+    play: require('numdu/assets/images/play.png'),
+    repeat: require('numdu/assets/images/repeat.png'),
+    send: require('numdu/assets/images/send.png'),
+    settings: require('numdu/assets/images/settings.png'),
+    user: require('numdu/assets/images/user.png'),
+    x: require('numdu/assets/images/x.png'),
+}
+
+export default ICONS;
