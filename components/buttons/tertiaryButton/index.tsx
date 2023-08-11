30b0130defe72ef1058a8ad38306497f727d90be
Itay Barzelai <itay1ba2@gmail.com>
2023-08-11 19:54:06 +0300

    tertiaryButton component

---
+++ components/buttons/tertiaryButton/index.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, Pressable } from "react-native";
+import React from "react";
+import COLORS from "../../../colors";
+
+interface Props {
+  text: string;
+  onPress: () => void;
+}
+
+const TertiaryButton = ({ text, onPress }: Props) => {
+  return (
+    <Pressable style={styles.container} onPress={onPress}>
+      <Text style={styles.text}>{text}</Text>
+    </Pressable>
+  );
+};
+
+export default TertiaryButton;
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "center",
+    paddingHorizontal: 10,
+  },
+  text: {
+    color: COLORS.WHITE,
+    textAlign: "center",
+    fontFamily: "rubikMedium",
+    fontSize: 20,
+    paddingVertical: 11,
+    textDecorationLine: "underline",
+  },
+});
