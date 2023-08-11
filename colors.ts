271ecdc647ed5557d099d0bb08b28eb1a412e063
Itay Barzelai <itay1ba2@gmail.com>
2023-08-11 22:42:27 +0300

    add and fix colors consts

--- colors.ts
+++ colors.ts
@@ -4,7 +4,8 @@ const COLORS = {
     GRAY: '#D0D0D0',
     DARKER_GRAY: '#878686',
     PRIMARY: '#0052CD',
-    SECONDARY: '00154A'
+    SECONDARY: '#00154A',
+    TINT: '#00000077'
 }
 
 export default COLORS;
