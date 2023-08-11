45e8b1af4136d0906dc9a7b631e0725b5369ef1f
Itay Barzelai <itay1ba2@gmail.com>
2023-08-11 19:54:16 +0300

    exitModal component

---
+++ components/modals/ExitModal/index.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import BlankModal from "../BlankModal";
+import COLORS from "../../../colors";
+import PrimaryButton from "../../buttons/PrimaryButton";
+import TertiaryButton from "../../buttons/tertiaryButton";
+
+interface Props {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+  onPressQuit: () => void;
+}
+
+const ExitModal = ({ visible, setVisible, onPressQuit }: Props) => {
+  return (
+    <BlankModal visible={visible} setVisible={setVisible}>
+      <Text style={styles.quitText}>You sure you want to quit?</Text>
+      <PrimaryButton text="No" onPress={() => setVisible(false)} />
+      <TertiaryButton text="Yes" onPress={onPressQuit} />
+    </BlankModal>
+  );
+};
+
+export default ExitModal;
+
+const styles = StyleSheet.create({
+  quitText: {
+    fontFamily: "rubikMedium",
+    fontSize: 20,
+    color: COLORS.WHITE,
+    textAlign: "center",
+  },
+});
